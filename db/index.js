ef5ea5a7c9c83f0cec79c84e89b23b92b8660013
Daniel Weiner <daweiner16@gmail.com>
2022-07-26 21:45:41 -0400

    update mysql connection

--- db/index.js
+++ db/index.js
@@ -2,9 +2,17 @@ var mysql = require('mysql');
 
 
 var connection = mysql.createConnection({
+  host: '3.88.49.253',
+  port: '3306',
   user: 'root',
   password: 'root',
-  database: 'sdc'
+  database: 'sdc',
+  ssl  : {
+    // DO NOT DO THIS
+    // set up your ca correctly to trust the connection
+    rejectUnauthorized: false
+  },
+
 });
 
 connection.connect(function(err) {
